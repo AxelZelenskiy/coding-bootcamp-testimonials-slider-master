de7c7b42e71f2d817d296e079e67b6ce09d7c7a5
AVZ <alex.zelenskiy@yahoo.com>
2020-06-17 18:33:26 +0300

    Start working on js-scripts of slider.

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,15 @@
 import './styles/main.scss';
+
+  let slides = document.querySelectorAll('.slide'),
+    buttonPrev = document.querySelector('.buttons :first-child'),
+    buttonNext = document.querySelector('.buttons :last-child');
+    console.log(buttonPrev === undefined,' 00000')
+  if (!slides.lenght === 0 || (buttonPrev !== undefined) || buttonNext !== undefined) {
+  	console.log('start')
+  		buttonPrev.addEventListener('click', (e) => {
+  			console.log('pushed prev button')
+  		});
+  		buttonNext.addEventListener('click', function(){
+  			console.log('pushed Next');
+  		});
+  } else { console.warn('Not found one of control or controlled elements');}
